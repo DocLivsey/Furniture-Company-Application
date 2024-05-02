312f4904bc1c509e7fc3ef8cd6bde21b24de9f58
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-05-03 01:52:50 +0300

    create Constants.cs class for contain project`s constants

---
+++ FurnitureCompanyApp/Constants.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace FurnitureCompanyApp
+{
+    public class Constants
+    {
+        public static readonly string LocalServer = "localhost";
+        public static readonly string Port = "5432";
+        public static readonly string Userid = "postgres";
+        public static readonly string Password = "190122";
+        public static readonly string DatabaseName = "furniture company";
+    }
+}
