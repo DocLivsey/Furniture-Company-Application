4924aa5900b704025287b10cfcca2749840a7b2b
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-05-02 09:59:26 +0300

    create test form for testing some features for general forms of app

---
+++ FurnitureCompanyApp/Test.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace FurnitureCompanyApp
+{
+    public partial class Test : Form
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+    }
+}
