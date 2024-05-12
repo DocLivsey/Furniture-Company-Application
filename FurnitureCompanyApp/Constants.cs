85226bf3667e8586fdd65ce631bcf4201c567cfe
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-05-12 03:38:40 +0300

    added two static classes and placed the same constants in the Connection class
    place table`s names and their columns from my db in DatabaseTable class

--- FurnitureCompanyApp/Constants.cs
+++ FurnitureCompanyApp/Constants.cs
@@ -1,13 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 namespace FurnitureCompanyApp
 {
     public class Constants
     {
-        public static readonly string LocalServer = "localhost";
-        public static readonly string Port = "5432";
-        public static readonly string Userid = "postgres";
-        public static readonly string Password = "190122";
-        public static readonly string DatabaseName = "furniture company";
+        public static class Connection
+        {
+            public static readonly string LocalServer = "localhost";
+            public static readonly string Port = "5432";
+            public static readonly string Userid = "postgres";
+            public static readonly string Password = "190122";
+            public static readonly string DatabaseName = "furniture company";
+        }
+        
+        public static class DatabaseTable
+        {
+            public static readonly string ReceivingInvoicesTable = "receiving_invoices";
+            public static readonly string ComponentsWarehouseTable = "components_warehouse";
+            public static readonly string InvoiceAndComponentLinkTable = "invoice_for_components";
+
+            public static readonly List<string> ReceivingInvoicesTableFields = new List<string>()
+            {
+                "_id", "order_date", "receiving_date", "delivery_cost", "manufacturing_cost", "components_count"
+            };
+
+            public static readonly List<string> ComponentsWarehouseTableFields = new List<string>()
+            {
+                "_id", "name", "manufacture_date", "amount"
+            };
+
+            public static readonly List<string> InvoiceAndComponentLinkTableFields = new List<string>()
+            {
+                "invoice_id", "component_id"
+            };
+        }
     }
 }
