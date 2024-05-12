8ed97074ed8761c1786aff1626b72dd159432b41
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-05-12 13:32:44 +0300

    add some buttons with events and some handlers for it

--- FurnitureCompanyApp/Test.Designer.cs
+++ FurnitureCompanyApp/Test.Designer.cs
@@ -31,17 +31,112 @@ namespace FurnitureCompanyApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] { "First", "Second" });
+            this.comboBox1.Location = new System.Drawing.Point(478, 207);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(194, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(478, 155);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(194, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(478, 181);
+            this.maskedTextBox1.Mask = "00/00/0000 90:00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(194, 20);
+            this.maskedTextBox1.TabIndex = 2;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(62, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 117);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "SELECT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(412, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(493, 321);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(574, 321);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Test
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
             this.Name = "Test";
-            this.Text = "Test";
+            this.Load += new System.EventHandler(this.Test_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button button4;
+
+        private System.Windows.Forms.Button button3;
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+
+        private System.Windows.Forms.ComboBox comboBox1;
 
         #endregion
     }
--- FurnitureCompanyApp/Test.cs
+++ FurnitureCompanyApp/Test.cs
@@ -1,12 +1,58 @@
+using System;
+using System.Data;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Npgsql;
 
 namespace FurnitureCompanyApp
 {
     public partial class Test : Form
     {
-        public Test()
+        private NpgsqlConnection Connection { get; set; }
+        
+        public Test(NpgsqlConnection connection)
         {
             InitializeComponent();
+            Connection = connection;
+        }
+
+        private void Test_Load(object sender, EventArgs e)
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QueryTools.UpdateTable("receiving_date = 'monday'", "_id = 1",
+                "receiving_invoices", Connection);
+            var map = QueryTools.SelectFromTableWhere("_id", "_id = 1",
+                "receiving_invoices", Connection);
+            foreach (var dict in map)
+                foreach (var pair in dict)
+                    Console.WriteLine(pair);
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ErrorProvider errorProvider = new ErrorProvider();
+            errorProvider.SetError(button2, "ERROR");
+            errorProvider.SetIconAlignment(button2, ErrorIconAlignment.BottomLeft);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
